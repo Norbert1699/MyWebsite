9849ceda1327ace797f6d8d21ae323064b6865e2
Norbert Kosinski <Norbert.Kosinski99@gmail.com>
2017-06-15 22:27:56 +0100

    Added tons of new features

---
+++ main.js
@@ -0,0 +1,9 @@
+require('https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js')
+
+
+function findOutMore(e) {
+  e.preventDefault();
+  $('html, body').animate({
+    scrollTop: $("#learnMoreContent").offset().top
+  }, 750);
+  }
